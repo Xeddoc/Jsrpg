75535b9adca8e7eeb7f1fc58e6e8eaa032695d44
Xeddoc <gaetan.nisteron@ynov.com>
2020-01-06 01:18:11 +0100

    version alpha, fin sans dialogues

--- scriptmenu.js
+++ scriptmenu.js
@@ -1,3 +1,4 @@
+//SELECTION PERSO
 
 function lesquen() {
     document.getElementById("character-box").style.backgroundImage = "url('https://risibank.fr/cache/stickers/d84/8427-full.jpg')";
@@ -126,7 +127,7 @@ setTimeout(function(){
 }, 1000);
 setTimeout(function(){
    $('#lever').show();
-}, 8500);
+}, 8000);
 
 document.body.style.backgroundImage='url("reveil.png")'
 let charaname = "Risitas";
@@ -206,15 +207,17 @@ function changefond() {
 
 }
 
+//CHOIX SORTIR
 
 function sortir() {
 
+document.body.style.backgroundImage='url("rue.jpg")'
 $(function() {
         setTimeout(function() { $("#citation").fadeOut(100); }, 100)
 
         });
 
-document.body.style.backgroundImage='url("rue.jpg")'
+
 
 setTimeout(function(){
    $('#attaque').show();
@@ -252,6 +255,7 @@ createDiag( individual );
 
 }
 
+//RESTER CHEZ SOI
 
 function rester() {
 document.body.style.backgroundImage='url("alerte.png")'
@@ -321,6 +325,8 @@ document.body.style.backgroundImage='url("rue.jpg")'
 
 }
 
+//ATTAQUE SPECIALE
+
 function specialattackfail() {
 setTimeout(function(){
    $('#dialog5').show();
@@ -367,8 +373,15 @@ setTimeout(function(){window.location.reload()}, 8000);
 
 }
 
+//ATTAQUE SIMPLE 
 
 function simpleattack() {
+
+    $('#assomme').fadeOut();
+    $('#lit').fadeOut();
+
+    let audio = document.getElementById("issou");
+        audio.play();
 
     let advhealth = document.getElementById("advhealth")
     advhealth.value -= 20; 
@@ -407,40 +420,718 @@ function simpleattack() {
 
 
     $(function() {
-            setTimeout(function() { $("#adversaire").fadeOut(4000); }, 4000)
+            setTimeout(function() { $("#adversaire").fadeOut(3500); }, 3500)
 
             });
 
 
-    setTimeout(function(){document.body.style.backgroundImage='url("black.png")'}, 8000);
+    setTimeout(function(){document.body.style.backgroundImage='url("black.png")'}, 7000);
     setTimeout(function(){document.body.style.backgroundImage='url("10.jpg")'}, 10000);
-    
+
+   
+    setTimeout(function(){
+           $('#choix4').show();
+        }, 11000);
+
     
 }
+
+//HOPITAL 
+
+
+//FUIR
+
+function partir() {
+document.body.style.backgroundImage='url("hop.jpg")'   
+
+    $("#choix4").fadeOut(100);
+
+
+    setTimeout(function(){
+           $('#hosto').show();
+        }, 400);
+
+        let dialogs = ['Vous trouvez une armure et une épée'],  
+            initial = 0;
+            individual = dialogs[initial].split('');
+
+        function createDiag ( dialog ) {
+
+          for(i = 0; i < dialog.length; i++) {
+            (function(i){
+
+              setTimeout(function(){
+                $('#dialog7').text($('#dialog7').text() +   dialog[i]);
+              }, 50*i);  
+
+            }(i));
+
+          }
+
+        }
+
+
+        createDiag( individual );
+
+ 
+
+
+
+} 
+
+//DEMANDER OU JE SUIS 
+
+
+function repondre() {
+
+    document.body.style.backgroundImage='url("10.jpg")'
+    
+    $("#choix4").fadeOut(100);
+    $("#dialogdoc").fadeOut(100);
+    
+
+    let dialogs = ['Vous avez été retrouvé inconscient. Le docteur vous restaure votre vie et vous donne une nouvelle capacité ultime : SAINTE CHANCLA'],  
+            initial = 0;
+            individual = dialogs[initial].split('');
+
+        function createDiag ( dialog ) {
+
+          for(i = 0; i < dialog.length; i++) {
+            (function(i){
+
+              setTimeout(function(){
+                $('#dialogdoc').text($('#dialogdoc').text() +   dialog[i]);
+              }, 50*i);  
+
+            }(i));
+
+          }
+
+        }
+
+
+        createDiag( individual );
+
+    
+
+
+}
+
+
+//ADVERSAIRE EXTREMISTE FEMINOVEGAN
+
+function porterarm() {
+    document.body.style.backgroundImage='url("sortiehop.jpg")'   
+    $("#hosto").fadeOut(100);
+
+    setTimeout(function(){
+           $('#sortie').show();
+        }, 400);
+
+    let dialogs = ['Une feministe-vegan, extremiste précisons-le, vous barre la route et vous attaque'],  
+            initial = 0;
+            individual = dialogs[initial].split('');
+
+        function createDiag ( dialog ) {
+
+          for(i = 0; i < dialog.length; i++) {
+            (function(i){
+
+              setTimeout(function(){
+                $('#dialog8').text($('#dialog8').text() +   dialog[i]);
+              }, 50*i);  
+
+            }(i));
+
+          }
+
+        }
+
+
+        createDiag( individual );
+
+
+
+}
+
+
+function attack() {
+
+    let audio = document.getElementById("issou");
+            audio.play();
+
+    let adv2health = document.getElementById("adv2health")
+    adv2health.value -= 20; 
+
+    setTimeout(function(){let myhealth = document.getElementById("myfinalhealth")
+    myhealth.value -= 30;}, 200);
+
+
+        if (adv2health.value == 0) {
+
+                document.body.style.backgroundImage='url("special.png")'
+
+                setTimeout(function(){
+                   document.body.style.backgroundImage='url("sortiehop.jpg")';
+                }, 3000);
+
+
+                $("#sortie").fadeOut(100);
+
+                setTimeout(function(){
+                   $('#dialog10').show();
+                }, 400);
+
+                let dialogs = ['Vous avez triomphé de l adversaire grâce à votre toute nouvelle puissance'],  
+                initial = 0;
+                individual = dialogs[initial].split('');
+
+                    function createDiag ( dialog ) {
+
+                        for(i = 0; i < dialog.length; i++) {
+                            (function(i){
+
+                          setTimeout(function(){
+                            $('#dialog10').text($('#dialog10').text() +   dialog[i]);
+                          }, 50*i);  
+
+                            }(i));
+
+                        }
+
+
+                    }
+
+                
+
+
+                createDiag( individual );
+
+                $('#versfin').show();
+
+            
+        }
+
+    if (myhealth.value == 0) {
+        
+        document.body.style.backgroundImage='url("gameover.jpg")'
+
+    }
+    
+}
+
+
+function specialattack() {
+
+    let audio = document.getElementById("yatangaki");
+                audio.play();
+
+    let adv2health = document.getElementById("adv2health")
+        adv2health.value -= 100; 
+
+
+        if (adv2health.value == 0) {
+
+            document.body.style.backgroundImage='url("special.png")'
+
+                    setTimeout(function(){
+                       document.body.style.backgroundImage='url("sortiehop.jpg")';
+                    }, 3000);
+
+
+                    $("#sortie").fadeOut(100);
+
+            setTimeout(function(){
+                   $('#dialog10').show();
+                }, 400);
+
+                let dialogs = ['Vous avez triomphé de l adversaire grâce à votre toute nouvelle puissance'],  
+                initial = 0;
+                individual = dialogs[initial].split('');
+
+                    function createDiag ( dialog ) {
+
+                        for(i = 0; i < dialog.length; i++) {
+                            (function(i){
+
+                          setTimeout(function(){
+                            $('#dialog10').text($('#dialog10').text() +   dialog[i]);
+                          }, 50*i);  
+
+                            }(i));
+
+                        }
+
+
+                    }
+
+
+                createDiag( individual );
+
+                $('#versfin').show();
+
+        }
+
+}
+
+
 
 
 
 //DISTRAIRE
 
 function distraire() {
+
+setTimeout(function(){
+           $('#dialogmma').show();
+        }, 400);
+document.body.style.backgroundImage='url("rue.jpg")'
+
+$("#attaque").fadeOut(100);
+
+let dialogs = ['Sans rigoler. Je pratique la MMA depuis maintenant 6 ans, de la boxe en parallèle depuis 7 ans, je pourrai. Ainsi que la musculation depuis 4 ans, 1m87 pour 86 kg...'],  
+            initial = 0;
+            individual = dialogs[initial].split('');
+
+        function createDiag ( dialog ) {
+
+          for(i = 0; i < dialog.length; i++) {
+            (function(i){
+
+              setTimeout(function(){
+                $('#dialogmma').text($('#dialogmma').text() +   dialog[i]);
+              }, 50*i);  
+
+            }(i));
+
+          }
+
+        }
+
+
+        createDiag( individual );
+
+
+
+setTimeout(function(){
+           $('#choixmma').show();
+        }, 4000);
 }
 
+//CONTINUER DISTRACTION
+
+function continuer() {
+    $("#dialogmma").fadeOut(100);
+    $("#choixmma").fadeOut(100);
+
+    setTimeout(function(){
+           $('#dialog9').show();
+        }, 100);
+
+    let dialogs = ['Miracle! Le saint pavé MMA a fait fuir l individu sans visage! Le policier vous remercie et vous conseille de faire attention'],  
+            initial = 0;
+            individual = dialogs[initial].split('');
+
+        function createDiag ( dialog ) {
+
+          for(i = 0; i < dialog.length; i++) {
+            (function(i){
+
+              setTimeout(function(){
+                $('#dialog9').text($('#dialog9').text() +   dialog[i]);
+              }, 50*i);  
+
+            }(i));
+
+          }
+
+        }
+
+
+        createDiag( individual );
+
+    setTimeout(function(){
+           $('#larrypolice').show();
+        }, 2000);
+
+    setTimeout(function(){
+           $('#continue2').show();
+        }, 3000);
+
+
+}
+
+//AVANCER DANS LA RUE
+
+function avance() {
+
+    document.body.style.backgroundImage='url("ruelle.jpg")'
+    $("#larrypolice").fadeOut(100);
+    $("#continue2").fadeOut(100);
+    $("#dialog9").fadeOut(100);
+    
+
+    setTimeout(function(){
+           $('#dialogruelle').show();
+        }, 100);
+
+    setTimeout(function(){
+           $('#choixinter').show();
+        }, 100);
+
+    let dialogs = ['Vous arrivez dans une ruelle très inquietante qui mène au parking privé de votre résidence, vous avez contourné votre immeuble par la rue précédente'],  
+            initial = 0;
+            individual = dialogs[initial].split('');
+
+        function createDiag ( dialog ) {
+
+          for(i = 0; i < dialog.length; i++) {
+            (function(i){
+
+              setTimeout(function(){
+                $('#dialogruelle').text($('#dialogruelle').text() +   dialog[i]);
+              }, 50*i);  
+
+            }(i));
+
+          }
+
+        }
+
+
+        createDiag( individual );
+
+
+    
+}
+
+
+// PARKING
+
+function park() {
+    
+    $("#dialogruelle").fadeOut(100);
+    $("#choixinter").fadeOut(100);
+    document.body.style.backgroundImage='url("park.jpg")'
+
+    setTimeout(function(){
+               $('#dialogparking').show();
+            }, 200);
+
+    setTimeout(function(){
+               $('#choixpark').show();
+            }, 100);
+
+    let dialogs = ['Vous cherchez votre voiture, elle était pourtant là...'],  
+                initial = 0;
+                individual = dialogs[initial].split('');
+
+            function createDiag ( dialog ) {
+
+              for(i = 0; i < dialog.length; i++) {
+                (function(i){
+
+                  setTimeout(function(){
+                    $('#dialogparking').text($('#dialogparking').text() +   dialog[i]);
+                  }, 50*i);  
+
+                }(i));
+
+              }
+
+            }
+
+
+            createDiag( individual );    
+      
+
+
+    
+
+
+}
+
+//RETOUR SUR SES PAS
+
+function retour() {
+    $("#dialogruelle").fadeOut(100);
+    $("#choixinter").fadeOut(100);
+
+
+    setTimeout(function(){
+               $('#gameov2').show();
+            }, 1500);
+    setTimeout(function(){
+               $('#gameover2').show();
+            }, 200);
+
+    let dialogs = ['Perdu.. Affronter ses peurs peut parfois nous sauver.. Vous vous évanouissez et perdez tout souvenir.'],  
+                initial = 0;
+                individual = dialogs[initial].split('');
+
+            function createDiag ( dialog ) {
+
+              for(i = 0; i < dialog.length; i++) {
+                (function(i){
+
+                  setTimeout(function(){
+                    $('#gameover2').text($('#gameover2').text() +   dialog[i]);
+                  }, 50*i);  
+
+                }(i));
+
+              }
+
+            }
+
+
+            createDiag( individual );    
+      
+
+
+
+}
+
+// LE HUMMER
+
+function carjack() {
+    document.body.style.backgroundImage='url("route.jpg")'
+
+    $("#dialogparking").fadeOut(100);
+    $("#choixpark").fadeOut(100);
+    
+    setTimeout(function(){
+               $('#hummerrouge').show();
+            }, 200);
+
+    let dialogs = ['VOUS VOLEZ LE HUMMER ROUGE, APPELEZ VOTRE AMI DE TOUJOURS JESUS ET PARTEZ A TRAVERS LE MONDE, DOMINER TERRES ET CIEUX, A BORD DE CET INCROYABLE VEHICULE'],  
+                initial = 0;
+                individual = dialogs[initial].split('');
+
+            function createDiag ( dialog ) {
+
+              for(i = 0; i < dialog.length; i++) {
+                (function(i){
+
+                  setTimeout(function(){
+                    $('#hummerrouge').text($('#hummerrouge').text() +   dialog[i]);
+                  }, 50*i);  
+
+                }(i));
+
+              }
+
+            }
+
+
+            createDiag( individual );  
+
+    setTimeout(function(){
+               $('#hummer').show();
+            }, 100);
+
+
+    
+}
+
+
+//ETAGE PARKING
+
+function etage() {
+    document.body.style.backgroundImage='url("black.png")'
+    $('#dialogparking').fadeOut();
+    $('#choixpark').fadeOut();
+
+    $('#assomme').show();
+    $('#lit').show();
+
+    let dialogs = ['Vous ressentez une grosse douleur à la tête, quelqu un vous assomme.. Vous tombez et perdez connaissance..'],  
+                initial = 0;
+                individual = dialogs[initial].split('');
+
+            function createDiag ( dialog ) {
+
+              for(i = 0; i < dialog.length; i++) {
+                (function(i){
+
+                  setTimeout(function(){
+                    $('#assomme').text($('#assomme').text() +   dialog[i]);
+                  }, 50*i);  
+
+                }(i));
+
+              }
+
+            }
+
+
+            createDiag( individual );    
+      
+}
 
 //CHOIX SOUS-JACENTS RESTER
 
 function barricader() {
-document.body.style.backgroundImage='url("")'
+    document.body.style.backgroundImage='url("gameover.jpg")'
 
-$(function() {
-        setTimeout(function() { $("#tv").fadeOut(300); }, 300)
+    $(function() {
+            setTimeout(function() { $("#tv").fadeOut(100); }, 100)
 
-    });
+        });
+    setTimeout(function(){
+               $('#gameover1').show();
+            }, 100);
+
+    setTimeout(function(){
+               $('#gameov').show();
+            }, 1500);
+
+    let dialogs = ['Perdu.. Affronter ses peurs peut parfois nous sauver.. Vous vous évanouissez et perdez tout souvenir.'],  
+                initial = 0;
+                individual = dialogs[initial].split('');
+
+            function createDiag ( dialog ) {
+
+              for(i = 0; i < dialog.length; i++) {
+                (function(i){
+
+                  setTimeout(function(){
+                    $('#gameover1').text($('#gameover1').text() +   dialog[i]);
+                  }, 50*i);  
+
+                }(i));
+
+              }
+
+            }
+
+
+            createDiag( individual );    
+      
     
-    //GAME OVER  DIALOGUE 4 : IL FAUT AFFRONTER SES PEURS
+}
+
+
+
+//VERSFIN
+
+function versfin() {
+
+    $("#dialog10").fadeOut(100);
+    $("#versfin").fadeOut(100);
+
+    setTimeout(function(){
+                       document.body.style.backgroundImage='url("black.png")';
+                    }, 1000);
+    
+    setTimeout(function(){
+                       document.body.style.backgroundImage='url("sortiehop.jpg")';
+                    }, 5000);
+
+    setTimeout(function(){
+                       document.body.style.backgroundImage='url("black.png")';
+                    }, 7000);
+
+    setTimeout(function(){
+                       document.body.style.backgroundImage='url("hop.jpg")';
+                    }, 9000);
+
+    setTimeout(function(){
+                       document.body.style.backgroundImage='url("black.png")';
+                    }, 11000);
+    
+    setTimeout(function(){
+                       document.body.style.backgroundImage='url("hop2.jpg")';
+                    }, 13000);
+
+    setTimeout(function(){
+                       document.body.style.backgroundImage='url("black.png")';
+                    }, 14000);
+
+    setTimeout(function(){
+                       document.body.style.backgroundImage='url("hop3.jpg")';
+                    }, 15000);
+
+    setTimeout(function(){
+                       document.body.style.backgroundImage='url("black.png")';
+                    }, 16000);
+
+    setTimeout(function(){
+                       document.body.style.backgroundImage='url("hop5.jpg")';
+                    }, 17000);
+
+    setTimeout(function(){
+                       document.body.style.backgroundImage='url("black.png")';
+                    }, 18000);
+
+    setTimeout(function(){
+                       document.body.style.backgroundImage='url("hop5.jpg")';
+                    }, 18500);
+
+    
+
+    setTimeout(function(){
+                       document.body.style.backgroundImage='url("black.png")';
+                    }, 19000);
+
+    
+
+    setTimeout(function(){
+                       document.body.style.backgroundImage='url("hop5.jpg")';
+                    }, 19500);
+
+    setTimeout(function(){
+               $('#hook').show();
+            }, 19500);
+
+    setTimeout(function(){
+                       document.body.style.backgroundImage='url("black.png")';
+                    }, 20000);
+
+    setTimeout(function(){
+               $('#hook').fadeOut();
+            }, 19950);
+
+    setTimeout(function(){
+                       document.body.style.backgroundImage='url("hop5.jpg")';
+                    }, 20500);
+
+    setTimeout(function(){
+               $('#hook').show();
+            }, 20500);
+
+    setTimeout(function(){
+                       document.body.style.backgroundImage='url("black.png")';
+                    }, 21000);
+
+    setTimeout(function(){
+               $('#hook').fadeOut();
+            }, 20950);
+
+    setTimeout(function(){
+                    document.body.style.backgroundImage='url("hop5.jpg")';
+                    }, 21500);
+
+    setTimeout(function(){
+               $('#hook').show();
+            }, 21500);
+
+
+    
+
 
 }
 
 
+
+
+
+
+//REJOUER 
+
+function tryagain() {
+    window.location.reload()
+}
 
 
 
